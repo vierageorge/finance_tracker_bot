04032f37ba9147e6242af7514668598fb7bb4f76
Jorge Viera <jorge.viera@zemoga.com>
2023-12-21 22:03:02 -0500

    Persisting all the attributes

--- bot/models/expense.py
+++ bot/models/expense.py
@@ -1,5 +1,6 @@
 
 from utils.persist import save_expense
+from datetime import datetime
 
 default_media = 'RappiCard'
 
@@ -9,15 +10,15 @@ media_mapping = {
 }
 
 class Expense:
-  def __init__(self, description: str, value: int, media: str = None, date: str = None, category: str = None, paid: bool = None):
+  def __init__(self, description: str, value: int, media: str = None, date: str = None, category: str = None):
     self.description = description
     self.value = value
 
     self.media = media_mapping.get(media, default_media)
-    self.paid = paid
-
-    self.date = date
+    self.paid = 'FALSE' if self.media == default_media else None
+    
+    self.date = date if date else datetime.now().strftime("%Y/%m/%d")
     self.category = category
 
   def save(self):
-    save_expense(self.value, self.media, self.description)
+    save_expense(self.media, self.description, self.value, self.date, self.paid)
--- bot/utils/persist.py
+++ bot/utils/persist.py
@@ -12,5 +12,5 @@ sheet_service = gspread.service_account(CRED_FILE)
 sheet_file = sheet_service.open_by_key(EXPENSE_SHEET_ID)
 worksheet = sheet_file.worksheet(SHEET_NAME)
 
-def save_expense(media, description, value):
-  worksheet.append_row(['Gasto','2023/12/20','REGALOS|Cumpleaños', media, description, value, None], value_input_option='USER_ENTERED')
+def save_expense(media: str, description: str, value: int, date: str, paid: str):
+  worksheet.append_row(['Gasto',date, None, media, description, value, paid], value_input_option='USER_ENTERED')
