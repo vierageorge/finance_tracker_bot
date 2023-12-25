65570a4367feab090686b6240a23a10b76592b1d
vierageorge <joviera@gmail.com>
2023-12-25 07:46:03 -0500

    displaying buttons

--- bot/main.py
+++ bot/main.py
@@ -2,9 +2,10 @@ import os
 import telebot
 from dotenv import load_dotenv
 from utils.validations import is_valid_user, is_valid_expense_message
-from utils.persist import get_total_debt
+from utils.persist import get_total_debt, get_categories
 from utils.message_processing import get_expense_components
 from utils.messages import GREETING_MESSAGE, NOT_VALID_USER_MESSAGE, HELP_MESSAGE
+from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 from models.expense import Expense
 import logging
 
@@ -26,6 +27,13 @@ def send_welcome(message: telebot.types.Message) -> None:
 def send_total(message: telebot.types.Message) -> None:
   total_debt = get_total_debt()
   bot.send_message(message.chat.id, f"Total debt: {total_debt}")
+
+@bot.message_handler(commands=['test'], func=is_valid_user)
+def test_method(message: telebot.types.Message) -> None:
+  markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True, row_width=1)
+  categories = get_categories()
+  markup.add(*[KeyboardButton(option) for option in categories])
+  bot.send_message(message.chat.id, 'Give me my data', reply_markup=markup)
 
 @bot.message_handler(commands=['help'], func=is_valid_user)
 def send_help_message(message: telebot.types.Message) -> None:
